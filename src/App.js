3c387c9b8849b20544602e861298bbf50f5b2621
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-03-21 19:52:24 +0500

    complete (minor things left)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./pages/Landing/Landing";
 import Explore from "./pages/Explore/Explore";
 import LeaderBoard from "./pages/LeaderBoard/LeaderBoard";
 import Blogs from "./pages/Blogs/Blogs";
+import BlogDetails from "./pages/BlogDetails/BlogDetails";
 import FAQs from "./pages/FAQs/FAQs";
 import Profile from "./pages/Profile/Profile";
 
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/explore" element={<Explore />} />
         <Route exact path="/leader-board" element={<LeaderBoard />} />
         <Route exact path="/blogs" element={<Blogs />} />
+        <Route exact path="/blog-details" element={<BlogDetails />} />
         <Route exact path="/faqs" element={<FAQs />} />
         <Route exact path="/profile" element={<Profile />} />
       </Routes>
